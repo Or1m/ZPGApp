661349c0c99b42601388e3d68a7a21a067bb73b0
Your Name <you@example.com>
2020-10-21 22:37:45 +0200

    Fakt neviem zatial fuck it

--- ZPGApp/Scene.cpp
+++ ZPGApp/Scene.cpp
@@ -1,11 +1,21 @@
 #include "Scene.h"
 #include "Renderer.h"
 #include "Application.h"
+#include "Resources/Models/sphere.h"
 
 Scene::Scene(std::vector<Object*>& objects, std::vector<Light*>& lights, float test, float step, glm::vec3 V, glm::vec3 vectors[]) 
 :	objects(objects), lights(lights), test(test), step(step), V(V), vectors(vectors) {}
 
 void Scene::runScene(Window* window) {
+	std::string shaderPath = "Resources/Shaders/Phong.shader";
+
+	Object* object = new Object(sphere, sphereCount, NULL, NULL, false, shaderPath);
+
+	object->useShaderProgram();
+	object->init();
+
+	object->changeColor(V);
+
 	while (window->windowShouldNotClose()) {
 		Renderer::getInstance()->clear();
 
@@ -20,7 +30,7 @@ void Scene::runScene(Window* window) {
 			Renderer::getInstance()->draw(*this->objects[0]);
 		}*/
 
-		Renderer::getInstance()->draw(*this->objects[0]);
+		Renderer::getInstance()->draw(*object);
 
 		window->pollEvents();
 		window->swapBuffer();
